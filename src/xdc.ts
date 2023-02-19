8574aa7dc61132422123469400f15110cc3afbcc
billy93 <billyfebram@gmail.com>
2023-02-20 06:33:38 +0700

    update graph

--- src/xdc.ts
+++ src/xdc.ts
@@ -1,14 +1,27 @@
-import { log } from '@graphprotocol/graph-ts';
+import { log, ethereum, Value } from '@graphprotocol/graph-ts';
 import {
     Transfer
 } from '../generated/LensHub/ERC20';
 import {
-    Test
+    Account
 } from '../generated/schema';
 import { Address, BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 
+
 export function handleTransfer(event: Transfer): void {
-  log.info('Event [Transfer] has been trigger Value: {} Tx: {}', [
-    event.params.value.toString(),
-    event.transaction.hash.toHex()]);
-}
+    let fromAccount = Account.load(event.params.from.toHex());
+    if (fromAccount == null) {
+      fromAccount = new Account(event.params.from.toHex());
+      fromAccount.balance = BigInt.fromI32(0);
+    }
+    fromAccount.balance = fromAccount.balance.minus(event.params.value);
+    fromAccount.save();
+  
+    let toAccount = Account.load(event.params.to.toHex());
+    if (toAccount == null) {
+      toAccount = new Account(event.params.to.toHex());
+      toAccount.balance = BigInt.fromI32(0);
+    }
+    toAccount.balance = toAccount.balance.plus(event.params.value);
+    toAccount.save();
+  }
